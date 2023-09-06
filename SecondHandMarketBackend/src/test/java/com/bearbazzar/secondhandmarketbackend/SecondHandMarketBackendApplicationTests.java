7532fdbd024aa69a08cba7c2d6178db00354833c
jiezhongXiaoShawn <112424103+jiezhongXiaoShawn@users.noreply.github.com>
2023-09-05 21:58:40 -0500

    fix no java

---
+++ SecondHandMarketBackend/src/test/java/com/bearbazzar/secondhandmarketbackend/SecondHandMarketBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bearbazzar.secondhandmarketbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondHandMarketBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
