34f9459d098eed10cd43d1b1cde51dcefee3819d
ryan <yongyan@wustl.edu>
2023-09-26 14:38:56 -0500

    [9.25.1] P0: Commented frontend files

--- frontend/src/userPanel/userPanel.js
+++ frontend/src/userPanel/userPanel.js
@@ -3,6 +3,7 @@ import * as Const from '../const';
 import Login from './login';
 import Register from './register';
 
+//"&&" works as if statement and deals with use input passed in the function
 function UserPanel({onClose, selectedUserPanel}) {
   const [selectedPage, setSelectedPage] = useState();
   return (
@@ -10,8 +11,7 @@ function UserPanel({onClose, selectedUserPanel}) {
         <div className="user-panel-content">
           <button onClick={onClose}>close</button>
           {selectedUserPanel === Const.LOGIN && <Login onClose={onClose}/>}
-          {selectedUserPanel === Const.REGISTER &&
-              <Register onClose={onClose}/>}
+          {selectedUserPanel === Const.REGISTER && <Register onClose={onClose}/>}
         </div>
       </div>
   );
