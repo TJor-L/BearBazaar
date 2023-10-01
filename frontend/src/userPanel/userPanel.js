331196b4121263b1e3a915eb5cfd91f1e6ee1b06
ZacharyWWen <hongzhe.w@wustl.edu>
2023-09-30 23:50:09 -0500

    [9.30.1] P0.5: added logout and wrote searchPanel

--- frontend/src/userPanel/userPanel.js
+++ frontend/src/userPanel/userPanel.js
@@ -1,19 +1,29 @@
-import {useState} from 'react';
+import {useState, useContext} from 'react';
 import * as Const from '../const';
 import Login from './login';
 import Register from './register';
+import UserContext from '../contexts/userContext';
 
-//"&&" works as if statement and deals with use input passed in the function
 function UserPanel({onClose, selectedUserPanel}) {
   const [selectedPage, setSelectedPage] = useState();
+  const { clearUser } = useContext(UserContext); 
+
+  const handleLogout = () => {
+    clearUser(); // Clear the user context
+    onClose(); 
+  };
+
   return (
-      <div className="user-panel-overlay">
-        <div className="user-panel-content">
-          <button onClick={onClose}>close</button>
-          {selectedUserPanel === Const.LOGIN && <Login onClose={onClose}/>}
-          {selectedUserPanel === Const.REGISTER && <Register onClose={onClose}/>}
-        </div>
+    <div className="user-panel-overlay">
+      <div className="user-panel-content">
+        <button onClick={onClose}>close</button>
+        {selectedUserPanel === Const.LOGIN && <Login onClose={onClose}/>}
+        {selectedUserPanel === Const.REGISTER && <Register onClose={onClose}/>}
+        {selectedUserPanel !== Const.LOGIN && selectedUserPanel !== Const.REGISTER && (
+          <button onClick={handleLogout}>Logout</button>
+        )}
       </div>
+    </div>
   );
 }
 
