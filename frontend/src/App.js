d3c3573417168bd760994eef906040eeb5c25e3d
ryan <yongyan@wustl.edu>
2023-10-31 19:48:38 -0500

    [10.31.1] Added session key

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,10 @@ import logo from './logo.png';
 import './App.css';
 import Navigation from './navigation';
 import UserPanel from './userPanel/userPanel';
-import {useState} from 'react';
+import {useContext, useState} from 'react';
 import * as Const from './const';
 import UserContext from './contexts/userContext';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
 
 import UserPage from './pages/user';
 import HomePage from './pages/home';
@@ -19,10 +19,21 @@ import { Layout} from 'antd';
 const { Header, Content } = Layout;
 
 function App() {
-  //initialization: page at home, user panel closed
-  const [selectedUserPanel, setSelectedUserPanel] = useState(Const.CLOSE);
-  const [contextUsername, setContextUsername] = useState('');
-  const [contextUserID, setContextUserID] = useState('');
+    const storedTokenSession = sessionStorage.getItem('authToken');
+    const storedUsernameSession = sessionStorage.getItem('username');
+    const storedUserIDSession = sessionStorage.getItem('userID');
+
+    const storedTokenLocal = localStorage.getItem('authToken');
+    const storedUsernameLocal = localStorage.getItem('username');
+    const storedUserIDLocal = localStorage.getItem('userID');
+
+    const initialToken = storedTokenSession || storedTokenLocal;
+    const initialUsername = storedUsernameSession || storedUsernameLocal;
+    const initialUserID = storedUserIDSession || storedUserIDLocal;
+
+    const [selectedUserPanel, setSelectedUserPanel] = useState(Const.CLOSE);
+    const [contextUsername, setContextUsername] = useState(initialUsername || '');
+    const [contextUserID, setContextUserID] = useState(initialUserID || '');
 
   //open selected user panel
   function handleOnUserPanelClick(panelName) {
@@ -33,6 +44,24 @@ function App() {
   function handleCloseUserPanelClick() {
     setSelectedUserPanel(Const.CLOSE);
   }
+
+    function PrivateRoute({ children }) {
+        let isAuthenticated = checkAuthFunction(); // replace with your auth check
+
+        if (!isAuthenticated) {
+            return <Navigate to="/login" />;
+        }
+
+        return children;
+    }
+
+    function checkAuthFunction() {
+        // Check if an auth token exists in local or session storage
+        const token = localStorage.getItem('authToken') || sessionStorage.getItem('authToken');
+
+        // Return true if a token exists, otherwise false
+        return !!token;
+    }
 
     return (
         <UserContext.Provider value={{ contextUsername, setContextUsername, contextUserID, setContextUserID }}>
@@ -47,10 +76,10 @@ function App() {
                             <Route path="/user/:urlUserID" element={<UserPage />} />
                             <Route path="/home" element={<HomePage />} />
                             <Route path="/searching" element={<SearchingPage />} />
-                            <Route path="/item/:itemID" element={<ItemPage/>} />
-                            <Route path="/postitem" element={<PostItem/>} />
-                            <Route path="/posted-item" element={<PostedItems/>} />
-                            <Route path="/transactions" element={<Transactions/>} />
+                            <Route path="/item/:itemID" element={<PrivateRoute><ItemPage/></PrivateRoute>} />
+                            <Route path="/postitem" element={<PrivateRoute><PostItem/></PrivateRoute>} />
+                            <Route path="/posted-item" element={<PrivateRoute><PostedItems/></PrivateRoute>} />
+                            <Route path="/transactions" element={<PrivateRoute><Transactions/></PrivateRoute>} />
                             <Route path="/" element={<HomePage />} exact />
                         </Routes>
                     </Content>
