6dd6bc8d02318ca5339577f93ceab9fda8a49bd5
jiezhongXiaoShawn <112424103+jiezhongXiaoShawn@users.noreply.github.com>
2023-11-05 22:07:21 -0600

    aaa

--- backend/src/main/java/com/bearbazzar/secondhandmarketbackend/controller/UserController.java
+++ backend/src/main/java/com/bearbazzar/secondhandmarketbackend/controller/UserController.java
@@ -48,6 +48,14 @@ public class UserController {
         }
         return ResponseEntity.ok(updatedUser);
     }
+    @GetMapping("/{username}")
+    public User getUserByUsername(@PathVariable String username) {
+        User user = UserService.getUserByUsername(username);
+        if(user == null){
+            throw new UserNotExistException("User No exist");
+        }
+        return user;
+    }
     @GetMapping("/user/{id}")
     public User getUserById(@PathVariable Long id) {
         User user = UserService.getUserByStudentId(id);
