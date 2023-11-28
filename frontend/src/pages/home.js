a8f6e2f285acba6663f522af02ffceb69240f52a
ZacharyWWen <hongzhe.w@wustl.edu>
2023-11-27 20:22:33 -0600

    [11.27.1] P2: card size fixes

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import { Card, Layout, List, Typography } from 'antd'
 import { Link } from 'react-router-dom'
-
+import fakeItems from '../fakedata/fakeItems';
+import fakeItemMap from '../fakedata/fakeItemsMap';
+ 
 const apiUrl = process.env.REACT_APP_BACKEND_URL || 'http://localhost'
 const apiPort = process.env.REACT_APP_BACKEND_PORT || '8080'
 
@@ -9,26 +11,29 @@ const { Content } = Layout
 
 function HomePage () {
     const [items, setItems] = useState([])
-
+    const cardStyle = {
+        width: 350, 
+        height: 500,
+        overflow: 'hidden'
+    };
+    
     useEffect(() => {
-        // Fetch actual data from the backend
         const fetchData = async () => {
             try {
-                const response = await fetch(`${apiUrl}:${apiPort}/items`)
-
+                const response = await fetch(`${apiUrl}:${apiPort}/items`);
                 if (!response.ok) {
-                    throw new Error('Network response was not ok')
+                    throw new Error('Network response was not ok');
                 }
-
-                const data = await response.json()
-                setItems(data)
+                const data = await response.json();
+                setItems(data);
             } catch (error) {
-                console.error('There was a problem fetching the items:', error)
+                console.error('There was a problem fetching the items:', error);
             }
         }
 
-        fetchData()
-    }, [])
+        fetchData();
+    }, []);
+
 
     return (
         <Layout style={{ minHeight: '100vh' }}>
@@ -38,13 +43,14 @@ function HomePage () {
                     dataSource={items}
                     renderItem={item => (
                         <List.Item>
-                            <Link to={`/item/${item.id}`}>
+                            <Link to={`/item/${item.itemID}`}>
                                 <Card
                                     hoverable
-                                    cover={<img alt={item.name} src={item.image.length === 0 ? 'https://via.placeholder.com/150' : item.image[0].url} />}
+                                    style={cardStyle}
+                                    cover={<img alt={item.name} src={item.image.length === 0 ? 'https://via.placeholder.com/150' : item.image[0].url} style={{ width: '100%', height: 'auto' }} />}
                                 >
-                                    <Typography.Title level={4}>{item.name}</Typography.Title>
-                                    <Typography.Text>Estimated Price: ${item.price}</Typography.Text>
+                                    <Typography.Title level={4}>{item.itemName}</Typography.Title>
+                                    <Typography.Text>Estimated Price: ${item.estimatedPrice}</Typography.Text>
                                 </Card>
                             </Link>
                         </List.Item>
